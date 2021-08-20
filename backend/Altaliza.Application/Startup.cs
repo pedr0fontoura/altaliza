c006833f2ff297fea5cf5694cc021ac7325ba302
pedr0fontoura <pedrofontoura@id.uff.br>
2021-08-20 04:53:33 -0300

    fix(backend): update target framework (oops)

--- backend/Altaliza.Application/Startup.cs
+++ backend/Altaliza.Application/Startup.cs
@@ -30,10 +30,7 @@ namespace Altaliza.Application
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers().AddNewtonsoftJson(options =>
-            {
-                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-            });
+            services.AddControllers();
 
             services.AddDbContext<MySqlContext>();
 
